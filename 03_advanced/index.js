3d16fbcd17e65abdb902f70034e45715a4d400a1
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-07-05 17:24:18 -0300

    att

--- 03_advanced/index.js
+++ 03_advanced/index.js
@@ -4,4 +4,19 @@ let numbers = [1, 2, 3];
 numbers.push(5);
 console.log(numbers[2]);
 const names = ["Rafael", "Conrado"];
-names.push(4);
+//names.push(4)
+// 2- Sintaxe Array
+const nums = [100, 200];
+nums.push(300);
+console.log(nums);
+console.log(nums[0]);
+// 3- Any
+const arr1 = [1, "teste", true, [], { nome: "Rafael" }];
+console.log(arr1);
+arr1.push([1, 2, 3]);
+console.log(arr1);
+// 4- Parametros tipados
+function soma(a, b) {
+    console.log(a + b);
+}
+soma(5, 10);
--- 03_advanced/index.ts
+++ 03_advanced/index.ts
@@ -7,5 +7,29 @@ console.log(numbers[2])
 
 const names: string[] = ["Rafael", "Conrado"]
 
-names.push(4)
+//names.push(4)
 
+// 2- Sintaxe Array
+const nums: Array<number> = [100, 200]
+
+nums.push(300)
+
+console.log(nums)
+
+console.log(nums[0])
+
+// 3- Any
+const arr1: any = [1, "teste", true, [], {nome: "Rafael"}]
+
+console.log(arr1)
+
+arr1.push([1, 2, 3])
+
+console.log(arr1)
+
+// 4- Parametros tipados
+function soma(a: number, b: number){
+    console.log(a + b)
+}
+
+soma(5,10)
